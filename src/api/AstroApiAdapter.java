230fbd31883dc70494d983d8baeed16249aed0b8
SytivaWheeler <69877161+SytivaWheeler@users.noreply.github.com>
2020-11-30 13:34:33 -0500

    Add files via upload

---
+++ src/api/AstroApiAdapter.java
@@ -0,0 +1,23 @@
+package astroapi;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author sytiva
+ */
+public class AstroApiAdapter {
+    AstroApiInterface AstroAdapter = new AstroApiTranslator();
+
+    public String getBodyInfo(String _body, String _dataWanted){
+        return AstroAdapter.getBodyInfo(_body, _dataWanted);
+    }
+
+    public JSONObject getBodyInfo(String _body){
+        return AstroAdapter.getBodyInfo(_body);
+    }
+
+     public String[] getMoonsAsArray(String _body) {
+        return AstroAdapter.getMoonsAsArray(_body);
+    }
+}
