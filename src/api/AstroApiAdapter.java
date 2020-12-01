6ab5830c006844f39bfe33da25ff76416c57ab3c
SytivaWheeler <69877161+SytivaWheeler@users.noreply.github.com>
2020-12-01 13:36:43 -0500

    Update AstroApiAdapter.java

--- src/api/AstroApiAdapter.java
+++ src/api/AstroApiAdapter.java
@@ -8,21 +8,21 @@ import org.json.JSONObject;
  * @author sytiva
  */
 public class AstroApiAdapter {
-    AstroApiInterface AstroAdapter = new AstroApiTranslator();
+    private final AstroApiInterface astroAdapter = new AstroApiTranslator();
 
     public String getBodyInfo(String _body, String _dataWanted){
-        return AstroAdapter.getBodyInfo(_body, _dataWanted);
+        return astroAdapter.getBodyInfo(_body, _dataWanted);
     }
 
     public JSONObject getBodyInfo(String _body){
-        return AstroAdapter.getBodyInfo(_body);
+        return astroAdapter.getBodyInfo(_body);
     }
 
      public String[] getMoonsAsArray(String _body) {
-        return AstroAdapter.getMoonsAsArray(_body);
+        return astroAdapter.getMoonsAsArray(_body);
     }
 
     public ArrayList<String> getBodyMoons(String _body) {
-        return AstroAdapter.getBodyMoons(_body);
+        return astroAdapter.getBodyMoons(_body);
     }
 }
