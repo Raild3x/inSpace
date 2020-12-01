2643223c9f614cebedec1c320404570102db3ad7
SytivaWheeler <69877161+SytivaWheeler@users.noreply.github.com>
2020-12-01 15:20:17 -0500

    Update LocationApiAdapter.java

--- src/api/LocationApiAdapter.java
+++ src/api/LocationApiAdapter.java
@@ -6,10 +6,9 @@ package api;
  */
 public class LocationApiAdapter {
 
-    private final LocationApiInterface locationAdapter = new IPInfoApiTranslator();
+    private final LocationApiInterface locationAdapter = new LocationApiTranslator();
 
     public String getLocationInfo(String _placeInfo) {
         return locationAdapter.getLocationInfo(_placeInfo);
     }
-
 }
