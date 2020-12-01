8726616d548255c75b9508c164c6b2a420cddae0
SytivaWheeler <69877161+SytivaWheeler@users.noreply.github.com>
2020-11-30 21:06:48 -0500

    Update SunMoonRiseApiTranslator.java

--- src/api/SunMoonRiseApiTranslator.java
+++ src/api/SunMoonRiseApiTranslator.java
@@ -1,5 +1,6 @@
 package api;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -12,20 +13,21 @@ public class SunMoonRiseApiTranslator extends APIConnect implements SunMoonRiseA
     private final LocationApiAdapter LOCATIONDATA = new LocationApiAdapter();
     private final String LAT = LOCATIONDATA.getLocationInfo("Latitude");
     private final String LON = LOCATIONDATA.getLocationInfo("Longitude");
+    private static JSONObject OBJ;
 
     /**
      * Returns time expected for sunrise/set and moon rise/set based on the
-     * users location. Uses same API as IPGEOLocation. Strings allowed can be
+     * users location. Uses IPGEOLocation API. Strings allowed can be
      * found in the Info Accessible through API google doc.
      */
     public String getSunMoonInfo(String _event) {
         String url = ASTRONOMY_URL + API_KEY + "&lat=" + LAT + "&long=" + LON;
         String param = _event.toLowerCase();
-        getConnection(url);
+        OBJ = getConnection(url);
 
         try {
             if (OBJ.getString(param).equals("-:-")) {
-                return "No moonset / moonrise for this date.";
+                return "No set / rise for this date.";
             } else {
                 return OBJ.getString(param);
             }
@@ -34,4 +36,3 @@ public class SunMoonRiseApiTranslator extends APIConnect implements SunMoonRiseA
         }
     }
 }
-
